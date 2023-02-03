f4747733796e4ffb3f1d8fa3ed21748d41497be7
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-02-03 19:04:27 -0300

    11. Lista de productos

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,32 +1,37 @@
 import React, { useContext } from 'react';
-import '@styles/ProductItem.scss';
+// import '@styles/ProductItem.scss';
 import AppContext from '../context/AppContext';
-import addToCartImage from '@icons/bt_add_to_cart.svg';
-import addedToCartImage from '@icons/bt_added_to_cart.svg';
+import addToCartimg from '@icons/bt_add_to_cart.svg';
+import addedToCartimg from '@icons/bt_added_to_cart.svg';
+import styles from '@styles/ProductItem.module.scss';
+import Image from 'next/Image';
 
 const ProductItem = ({ product }) => {
-	const { state, addToCart } = useContext(AppContext);
+  const { state, addToCart } = useContext(AppContext);
 
-	const handleClick = item => {
-		console.log('in cart: ', state.cart.includes(item));
-		addToCart(item)
-	}
+  const handleClick = (item) => {
+    console.log('in cart: ', state.cart.includes(item));
+    addToCart(item);
+  };
 
-	return (
-		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
-			<div className="product-info">
-				<div>
-					<p>${product.price}</p>
-					<p>{product.title}</p>
-				</div>
-				<figure className="more-clickable-area" onClick={() => handleClick(product)} >
-					{  state.cart.includes(product) ? <img className="disabled add-to-cart-btn" src={addedToCartImage} alt="added to cart" />  : <img className="add-to-cart-btn pointer" src={addToCartImage} alt="add to cart" /> }
-					
-				</figure>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className={styles.ProductItem}>
+      <img src={product.images[0]} alt={product.title} />
+      <div className={styles['product-info']}>
+        <div>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
+        </div>
+        <figure className={styles['more-clickable-area']} onClick={() => handleClick(product)}>
+          {state.cart.includes(product) ? (
+            <img className={`${styles.disabled} ${styles['add-to-cart-btn']}`} src={addedToCartimg} alt="added to cart" />
+          ) : (
+            <img className={`${styles.pointer} ${styles['add-to-cart-btn']}`} src={addToCartimg} alt="add to cart" />
+          )}
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default ProductItem;
